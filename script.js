6151889870750ea9098b9312a133d615c2fbe6b0
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2024-04-16 23:38:30 +0200

    functions completetask, listalltasks, listcompletedtasks and remove tasks created

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ const taskManager = {
         this.askUserName();
       } else {
         alert(`Hello, ${this.userName}! Proceed to the menu`);
-        console.log(`Hello, ${this.userName}! Proceed to the menu`);
+       // console.log(`Hello, ${this.userName}! Proceed to the menu`);
       }
     },
 
 // To add tasks
     addTask: function() {
       const taskDescription = prompt("Please add a task description");
-      console.log("Please add a description");
+      //console.log("Please add a description");
       if(taskDescription.trim() === "") {
         alert("Task description cannot be empty!");
-        console.log("Task description cannot be empty!");
+       // console.log("Task description cannot be empty!");
         this.addTask();
       }
 // To get new task object 
@@ -38,12 +38,28 @@ const taskManager = {
       };
 // To add 'push' is used in task 
       this.tasks.push(task);
-      console.log("New task added");
+     // console.log("New task added");
       alert("Task is added!");
       menu();
     },
 
 // To mark the task as completed
+    completeTask: function () {
+        const showTaskId = parseInt(prompt("Enter the task ID"));
+        //console.log("enter the atsk id", showTaskId);
+        const taskFound = this.tasks.find((task) => task.id === showTaskId);
+
+        if (!taskFound) {
+            console.log("task not found");
+            alert("Task not found");
+            return;
+        }
+
+        taskFound.complete = true;
+        //console.log("task done");
+        alert("Task msrked as done!");
+        menu();
+    },
     
 // Display tasks list
     listAllTasks: function() {
@@ -63,8 +79,48 @@ const taskManager = {
                 "\n";
             })
         }
-        console.log("List of Tasks", message);
+        //console.log("List of Tasks", message);
         alert(message);
         menu();
-    }
+    },
+
+// Shows only those tasks are done
+    listCompletedTasks: function() {
+        let message = "";
+        const completedTasks = this.tasks.filter((task) => task.complete);
+
+        if(completedTasks.length === 0) {
+            message = "No completed tasks!";
+        } else {
+            message = "Completed Tasks\n";
+            completedTasks.forEach((task) => {
+                message +=
+                "Id:" +
+                task.id +
+                "Description" +
+                taskDescription +
+                "Complete:" +
+                task.complete +
+                "\n";
+            })
+        }
+        alert(message);
+        menu();
+    },
+    
+// Remove the selected task from list 
+    removeTask: function() {
+        const taskId = parseInt(prompt("Enter the ID of the task"));
+        const taskIndex = this.tasks.findIndex((task) => task.id === taskId);
+    
+        if(taskIndex === -1) {
+            alert("Task not found!");
+            menu();
+        }
+
+// 'splice' here is used to remove any task from the list     
+        this.tasks.splice(taskIndex, 1);
+        alert("Task is removed");
+        menu();    
+    },   
 }
